9534c951d0b2e59285f20d96f138989ebd1060c7
Franck Darren <kikondanze@gmail.com>
2024-11-14 18:03:52 +0100

    ajout dashboard count

---
+++ app/Livewire/DashboardCount.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Models\Souscription; // Assurez-vous d'importer le modèle
+
+class DashboardCount extends Component
+{
+    public $souscription_jour;
+    public $souscription_attentes;
+    public $souscription_totales;
+
+    public function render()
+    {
+        // Rafraîchir les données à chaque requête
+        $this->souscription_jour = Souscription::whereDate('created_at', now()->toDateString())->count();
+        $this->souscription_attentes = Souscription::where('statut', 'En attente de paiement')->count();
+        $this->souscription_totales = Souscription::count();
+
+        return view('livewire.dashboard-count', [
+            'souscription_jour' => $this->souscription_jour,
+            'souscription_attentes' => $this->souscription_attentes,
+            'souscription_totales' => $this->souscription_totales,
+        ]);
+    }
+}
---
+++ resources/views/livewire/dashboard-count.blade.php
@@ -0,0 +1,18 @@
+<div class="grid grid-cols-1 md:grid-cols-3 gap-5 justify-center" wire:poll.5s>
+    <div
+        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $souscription_jour }}</h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">Nombre de souscription du jour</p>
+    </div>
+    <div
+        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $souscription_attentes }}
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">Nombre de souscription en attente</p>
+    </div>
+    <div
+        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $souscription_totales }}</h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">Nombre total de souscription</p>
+    </div>
+</div>
