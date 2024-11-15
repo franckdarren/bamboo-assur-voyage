ee41f5fa007e2c483d86cd2a56d4dbbdbdb09597
Franck Darren <kikondanze@gmail.com>
2024-11-15 11:45:43 +0100

    maj dashboard

--- app/Livewire/DashboardCount.php
+++ app/Livewire/DashboardCount.php
@@ -10,12 +10,15 @@ class DashboardCount extends Component
     public $souscription_jour;
     public $souscription_attentes;
     public $souscription_totales;
+    public $souscription_encours_traitement;
+
 
     public function render()
     {
         // Rafraîchir les données à chaque requête
         $this->souscription_jour = Souscription::whereDate('created_at', now()->toDateString())->count();
         $this->souscription_attentes = Souscription::where('statut', 'En attente de paiement')->count();
+        $this->souscription_encours_traitement = Souscription::where('statut', 'En cours de traitement')->count();
         $this->souscription_totales = Souscription::count();
 
         return view('livewire.dashboard-count', [
--- resources/views/livewire/dashboard-count.blade.php
+++ resources/views/livewire/dashboard-count.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-1 md:grid-cols-3 gap-5 justify-center" wire:poll.5s>
+<div class="grid grid-cols-1 md:grid-cols-4 gap-5 justify-center" wire:poll.5s>
     <div
         class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $souscription_jour }}</h5>
@@ -12,7 +12,15 @@
     </div>
     <div
         class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $souscription_totales }}</h5>
+        <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {{ $souscription_encours_traitement }}
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">Souscriptions en cours de traitement</p>
+    </div>
+    <div
+        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $souscription_totales }}
+        </h5>
         <p class="font-normal text-gray-700 dark:text-gray-400">Total des souscriptions</p>
     </div>
 </div>
